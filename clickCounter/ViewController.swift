2015c22f41c5937b9970e78935603046ba0fb376
Maggie Moss <=>
2016-05-11 23:41:56 -0400

    adding a decrement button

--- clickCounter/ViewController.swift
+++ clickCounter/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     var count = 0
     var label: UILabel!
+    var secondLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +23,15 @@ class ViewController: UIViewController {
         label.frame = CGRectMake(150, 150, 60, 60)
         label.text = "0"
         
+        // second label
+        var secondLabel = UILabel()
+        secondLabel.frame = CGRectMake(150, 350, 60, 60)
+        secondLabel.text = "0"
+        
         self.view.addSubview(label)
+        self.view.addSubview(secondLabel)
         self.label = label
+        self.secondLabel = secondLabel
         // button
         
         var button = UIButton()
@@ -33,10 +41,27 @@ class ViewController: UIViewController {
         self.view.addSubview(button)
         
         button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        // decrement
+        var decrementButton = UIButton()
+        decrementButton.frame = CGRectMake(150, 450, 60, 60)
+        decrementButton.setTitle("Decrement", forState: .Normal)
+        decrementButton.setTitleColor(UIColor.cyanColor(), forState: .Normal)
+        self.view.addSubview(decrementButton)
+        
+        decrementButton.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
     }
     func incrementCount(){
         self.count += 1
         self.label.text = "\(self.count)"
+        self.secondLabel.text = "\(self.count)"
+
+    }
+    func decrementCount(){
+        self.count -= 1
+        self.label.text = "\(self.count)"
+        self.secondLabel.text = "\(self.count)"
     }
 }
 
